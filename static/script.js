a72a5d53c011d7bbf512f1a0064ab747e085dc87
krithikasb <76915822+krithikasb@users.noreply.github.com>
2022-04-18 23:34:49 +0530

    Handle newlines and spaces in the back of the postcard (#43)
    * favicon
    
    * pop db
    
    * remove image resize
    
    * remove comments
    
    * add update for sql
    
    * add script to create users
    
    * add line breaks to text area formatting
    
    * add script
    
    * handle spaces
    
    Co-authored-by: jobin212 <jobin212@gmail.com>

--- static/script.js
+++ static/script.js
@@ -208,7 +208,10 @@ window.onload = function () {
         }
         let formData = new FormData()
         formData.append("front-postcard-file", photo)
-        formData.append("back", backTextArea.value)
+        var backText = backTextArea.value;
+        backText = backText.replace(/( )/gm, '&#32;');
+        backText = backText.replace(/(\r\n|\n|\r)/gm, '<br />');
+        formData.append("back", backText)
         fetch("/postcards?isPreview=true&toRecurseId=0", { method: "POST", body: formData }).then(response =>
             response.json()
         ).then(data => {
